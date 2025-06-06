f4bdca6ff62c21743a590a8fb41cbbee0c2fe87e
unknown <joshua.falk@stud-mail.uni-wuerzburg.de>
2025-06-06 16:19:24 +0200

    improvments

--- src/main/java/com/tictactoe/tictactoe/GameState.java
+++ src/main/java/com/tictactoe/tictactoe/GameState.java
@@ -4,6 +4,8 @@ import com.tictactoe.tictactoe.gameWrapping.GamePhase;
 import com.tictactoe.tictactoe.gameWrapping.Outcome;
 import com.tictactoe.tictactoe.gameWrapping.PlayerSession;
 
+
+
 public record GameState(GamePhase gamePhase, int[][] board, PlayerSession winner, PlayerSession playerTurn,
                         PlayerSession playerOne, PlayerSession playerTwo, Outcome outcome) {
 }
